a7a2995ba4ecbd2214857eb92c9b189568fd297f
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-02-28 16:35:23 +0300

    Add project files.

---
+++ Homework 6-7/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Homework_6_7
+{
+    internal class Inventory
+    {
+        private List<Product> products;
+
+        public Inventory() 
+        {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public decimal GetPrice()
+        {
+            decimal price = 0;
+
+            foreach(var product in products)
+            {
+                price += product.Price * product.Quantity;
+            }
+            return price;
+        }
+    }
+}
---
+++ Homework 6-7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем продукты
+            Product product1 = new Product(1, "Product 1", 10.99m, 5);
+            Product product2 = new Product(2, "Product 2", 5.99m, 10);
+
+            // Создаем инвентарь
+            Inventory inventory = new Inventory();
+
+            // Добавляем продукты в инвентарь
+            inventory.AddProduct(product1);
+            inventory.AddProduct(product2);
+
+            // Рассчитываем общую стоимость инвентаря
+            decimal totalValue = inventory.GetPrice();
+
+            // Выводим результат
+            Console.WriteLine($"Total Inventory Value: {totalValue}");
+        }
+    }
+}
